2b373e7c59b8b62a9540f5437bb4ff9916cbe08b
ETIM-PAUL <etimpaul22@gmail.com>
2022-02-02 23:53:26 +0100

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import { fetchQuizQuestions } from './API'
+//components
+import QuestionCard from './components/QuestionCard';
+//types
+import { Difficulty} from './API';
 
-function App() {
+const TOTAL_QUESTIONS = 10;
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswer, setUserAnswer] = useState([]);
+  const [score, setscore] = useState(0);
+  const [quizOver, setQuizOver] = useState(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
+
+  const startTrivia = async () => {
+
+  };
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  };
+
+  const nextQuestion = () => {
+
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>QUIZ APP</h1>
+
+      <button className="start" onClick={startTrivia}>
+        Start Quiz
+      </button>
+
+      <p className='score'>Score:</p>
+      <p> Loading Questions ...</p>
+
+      {/* { <QuestionCard
+        questionNr={number + 1 }
+        totalQuestions = {TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswer ? userAnswer[number]: undefined}
+        callback={checkAnswer}
+      /> } */}
+      <button className="start" onClick={nextQuestion}>
+        Next Question
+      </button>
     </div>
   );
-}
+};
 
 export default App;
+ 
